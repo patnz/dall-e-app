85abff35868d336cdcbd02a3ff6868ccf7647932
Pat Wilson <pat@wilsons.org.nz>
2023-07-03 22:35:22 +1000

    added image/placeholder/loader to Post component

--- client/src/pages/Post.jsx
+++ client/src/pages/Post.jsx
@@ -47,6 +47,26 @@ export default function Post() {
             isSurpriseMe={handleSurpriseMe}
           />
         </div>
+        <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+          {form.photo ? (
+            <img
+              src={form.photo}
+              alt={form.prompt}
+              className="w-full h-full object-contain"
+            />
+          ) : (
+            <img
+              src={preview}
+              alt={preview}
+              className="w-9/12 h-9/12 object-contain opacity-40"
+            />
+          )}
+          {generatingImg && (
+            <div>
+              <Loader className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg" />
+            </div>
+          )}
+        </div>
       </form>
     </section>
   )
