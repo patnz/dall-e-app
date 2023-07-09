4febb33f2958535d3dca7c25040ea529f96c81d9
Pat Wilson <pat@wilsons.org.nz>
2023-07-09 22:44:00 +1000

    download image added

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import { download } from '../assets'
+import { downloadImage } from '../utils'
 
-export default function Card() {
+export default function Card({ _id, name, prompt, photo }) {
   return <div>Card</div>
 }
--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,4 +1,5 @@
 import surpriseMePrompts from '../constants/constants'
+import FileSaver from 'file-saver'
 
 export default function getRandomPrompt(prompt) {
   const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length)
@@ -9,4 +10,8 @@ export default function getRandomPrompt(prompt) {
   }
 
   return randomPrompt
+}
+
+export async function downloadImage(_id, photo) {
+  FileSaver.saveAs(photo, `download-${_id}.jpg`)
 }
