e18141c0fda8c223ccfd18d3b848a334a43bb206
Pat Wilson <pat@wilsons.org.nz>
2023-08-24 00:59:53 +1000

    updated mongo upload with quality param

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -31,12 +31,15 @@ router.route('/').get(async (req, res) => {
 router.route('/').post(async (req, res) => {
   try {
     const { name, prompt, photo } = req.body
-    const photoUrl = await cloudinary.uploader.upload(photo)
+    const uploadOptions = {
+      quality: 'eco', // Adjusted for economy!
+    }
+    const photoUrl = await cloudinary.uploader.upload(photo, uploadOptions)
 
     const newPost = await Post.create({
       name,
       prompt,
-      photo: photoUrl.url,
+      photo: photoUrl.url.replace('image/upload/', 'image/upload/q_25/'),
     })
 
     res.status(200).json({ success: true, data: newPost })
